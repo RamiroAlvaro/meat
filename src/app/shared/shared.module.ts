5136f635fdf5f883686631ab3cb28d2fd938ec3a
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2018-01-28 16:24:59 -0300

    Utilizando HTTP Interceptors Seção 15, aula 125

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {InputComponent} from './input/input.component';
 import {RadioComponent} from './radio/radio.component';
 import {RatingComponent} from './rating/rating.component';
@@ -12,6 +13,8 @@ import {NotificationService} from './messages/notification.service';
 import {LoginService} from '../security/login/login.service';
 import {LoggedInGuard} from '../security/loggedin.guard';
 import {LeaveOrderGuard} from '../order/leave-order.guard';
+import {AuthInterceptor} from '../security/auth.interceptor';
+
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -29,7 +32,8 @@ export class SharedModule {
                   NotificationService,
                   LoginService,
                   LoggedInGuard,
-                  LeaveOrderGuard]
+                  LeaveOrderGuard,
+                 {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
     };
   }
 }
